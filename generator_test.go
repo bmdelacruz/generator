f5b7ead1e8c57a37876054c3e6280058a43fd322
Bryan Dela Cruz <bryanmdlx@gmail.com>
2019-09-16 06:58:49 +0800

    Added tests for `Generator` struct
    - Created tests for `Next` and `Return` functions

--- generator_test.go
+++ generator_test.go
@@ -2,9 +2,76 @@ package generator_test
 
 import (
 	"fmt"
+	"testing"
 
 	"github.com/bmdelacruz/generator"
 )
+
+func TestGenerator_Next(t *testing.T) {
+	t.Run("Call_with_nil_on_empty_generator", func(t *testing.T) {
+		g := generator.New(
+			func(gc *generator.Controller) interface{} {
+				return nil
+			},
+		)
+		value, isDone, err := g.Next(nil)
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+		value, isDone, err = g.Next(nil)
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("Call_with_non-nil_on_empty_generator", func(t *testing.T) {
+		g := generator.New(
+			func(gc *generator.Controller) interface{} {
+				return nil
+			},
+		)
+		value, isDone, err := g.Next("a")
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+		value, isDone, err = g.Next("b")
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+	})
+}
+
+func TestGenerator_Return(t *testing.T) {
+	t.Run("Call_with_nil_on_empty_generator", func(t *testing.T) {
+		g := generator.New(
+			func(gc *generator.Controller) interface{} {
+				return nil
+			},
+		)
+		value, isDone, err := g.Return(nil)
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+		value, isDone, err = g.Return(nil)
+		if value != nil || !isDone || err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("Call_with_non-nil_on_empty_generator", func(t *testing.T) {
+		g := generator.New(
+			func(gc *generator.Controller) interface{} {
+				return nil
+			},
+		)
+		value, isDone, err := g.Return("a")
+		if value != "a" || !isDone || err != nil {
+			t.Fail()
+		}
+		value, isDone, err = g.Return("b")
+		if value != "b" || !isDone || err != nil {
+			t.Fail()
+		}
+	})
+}
 
 func ExampleGenerator_Return_without_value() {
 	g := generator.New(
