bd977a18d6b09b6c18a765aa495ebfc41007a159
Bryan Dela Cruz <bryanmdlx@gmail.com>
2019-09-18 23:12:02 +0800

    Updated generator internals
    - Refactored controller functions

--- controller.go
+++ controller.go
@@ -7,47 +7,26 @@ type Controller struct {
 }
 
 func (c *Controller) Yield(value interface{}) (interface{}, bool, error) {
-	if !c.wasUsed {
-		c.wasUsed = true
-	}
-
-	select {
-	case fc, ok := <-c.g.firstCallChan:
-		if ok {
-			switch fc.Type() {
-			case "return":
-				value, _ := fc.Values()
-				return value, true, nil
-			case "error":
-				_, err := fc.Values()
-				c.g.statusChan <- &status{
-					value: nil,
-					done:  false,
-					err:   nil,
-				}
-				<-c.g.retStatusChan
-				<-c.g.isDoneChan
-				return nil, false, err
-			}
-		}
-	default:
-	}
-
-	if c.g.isDone {
-		return nil, true, nil
-	}
-
-	c.g.statusChan <- &status{
-		value: value,
-		done:  false,
-		err:   nil,
-	}
-	rs := <-c.g.retStatusChan
-	<-c.g.isDoneChan
-	return rs.Data()
+	return c.sendAndReceive(
+		&status{
+			value: value,
+			done:  false,
+			err:   nil,
+		},
+	)
 }
 
 func (c *Controller) Error(err error) (interface{}, bool, error) {
+	return c.sendAndReceive(
+		&status{
+			value: nil,
+			done:  false,
+			err:   err,
+		},
+	)
+}
+
+func (c *Controller) sendAndReceive(statusToSend *status) (interface{}, bool, error) {
 	if !c.wasUsed {
 		c.wasUsed = true
 	}
@@ -78,11 +57,7 @@ func (c *Controller) Error(err error) (interface{}, bool, error) {
 		return nil, true, nil
 	}
 
-	c.g.statusChan <- &status{
-		value: nil,
-		done:  false,
-		err:   err,
-	}
+	c.g.statusChan <- statusToSend
 	rs := <-c.g.retStatusChan
 	<-c.g.isDoneChan
 	return rs.Data()
